0ba19048b104722f7007ff1d08088834f04557e7
doku2684 <55175860+doku2684@users.noreply.github.com>
2022-04-16 12:56:12 +0900

    Created bapgap.py

---
+++ bapgap.py
@@ -0,0 +1,5 @@
+class Bapgap:
+  def calculate(namesAndRawPrices, afterTax):
+      beforeTax = sum(array)
+      for (name, rawPrice) in namesAndRawPrices:
+          print(name, ": ", afterTax/beforeTax*rawPrice)
